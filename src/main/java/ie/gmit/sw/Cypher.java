5d0f3f6905c3c8d75c7eb5a20118d436e1c4eebb
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-02-06 22:11:31 +0000

    ADD, IMPLEMENT, TEST: getLetter()

--- src/main/java/ie/gmit/sw/Cypher.java
+++ src/main/java/ie/gmit/sw/Cypher.java
@@ -35,6 +35,10 @@ public class Cypher {
 
     /**
      * Method creates unique, 25 character long keyword.
+     *
+     * Time complexity: O(n) - linear
+     * Space complexity:
+     *
      * @param alphabet will be used to fill missing letters in keyword (order it appears matters).
      * @param keyWords will be used FIRST for generating keyword.
      * @return unique 25 character long created keyword.
@@ -54,7 +58,30 @@ public class Cypher {
                     .collect(Collectors.joining());
     }
 
-    static String toDigraphs(String s){
-        Queue<char[]> diagraphs = // https://stackoverflow.com/a/25441208/5322506
+//    static String toDigraphs(String s){
+//        Queue<char[]> diagraphs = // https://stackoverflow.com/a/25441208/5322506
+//    }
+
+    /**
+     * <hr>
+     * <p>Method gets letter from alphabet matrix.</p>
+     * <ul style="list-style-type:none">
+     *      <li>A B C D E</li>
+     *      <li>F G H I K</li>
+     *      <li>L M N O P</li>
+     *      <li>Q R S T U</li>
+     *      <li>V W X Y Z</li>
+     * </ul>
+     * <p><strong>NOTE: Here is no letter 'J' in Alphabet.</strong></p>
+     * <hr>
+     * <i>Time complexity: <b>O(1)</b> - constant, <br> Space complexity: <b>O(0)</b> - none.</i>
+     *
+     * @param x letters X position in Alphabet matrix starting from 0 ("zero").
+     * @param y letters Y position in Alphabet matrix starting from 0 ("zero").
+     * @return letter from matrix at position (x,y) or "space" if not valid X or Y.
+     */
+    static char getLetter(int x, int y){
+        if(0 > x || x > 4 || 0 > y || y > 4){ return ' '; } // if X or Y are off limits
+        return (char)(5 * x + y + 65 + (x > 1 || x == 1 && y == 4 ? 1 : 0));
     }
 }
--- src/test/java/ie/gmit/sw/CypherTest.java
+++ src/test/java/ie/gmit/sw/CypherTest.java
@@ -8,6 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CypherTest {
     @Test
+    void getLetter() {
+        assertAll(
+                ()-> assertEquals('A', Cypher.getLetter(0,0)),
+                ()-> assertEquals('E', Cypher.getLetter(0,4)),
+                ()-> assertEquals('I', Cypher.getLetter(1,3)),
+                ()-> assertEquals('K', Cypher.getLetter(1,4)),
+                ()-> assertEquals('L', Cypher.getLetter(2,0)),
+                ()-> assertEquals('P', Cypher.getLetter(2,4)),
+                ()-> assertEquals('Z', Cypher.getLetter(4,4)),
+                ()-> assertEquals(' ', Cypher.getLetter(-1,4)),
+                ()-> assertEquals(' ', Cypher.getLetter(0,5)),
+
+                ()-> {
+                    int x = 2, y = 2; // 'H'
+                    assertFalse('Z' == Cypher.getLetter(x,y), "Z != "+ Cypher.getLetter(x,y));
+                }
+        );
+
+    }
+
+    @Test
     void generateKeyword() {
 
 
