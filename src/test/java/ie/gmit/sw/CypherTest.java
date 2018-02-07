aa3bc770ea699208571ac83053815e021cfe6b96
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-02-07 01:19:06 +0000

    ADD: class Position

--- src/test/java/ie/gmit/sw/CypherTest.java
+++ src/test/java/ie/gmit/sw/CypherTest.java
@@ -2,27 +2,25 @@ package ie.gmit.sw;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.function.Predicate;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CypherTest {
     @Test
     void getLetter() {
         assertAll(
-                ()-> assertEquals('A', Cypher.getLetter(0,0)),
-                ()-> assertEquals('E', Cypher.getLetter(0,4)),
-                ()-> assertEquals('I', Cypher.getLetter(1,3)),
-                ()-> assertEquals('K', Cypher.getLetter(1,4)),
-                ()-> assertEquals('L', Cypher.getLetter(2,0)),
-                ()-> assertEquals('P', Cypher.getLetter(2,4)),
-                ()-> assertEquals('Z', Cypher.getLetter(4,4)),
-                ()-> assertEquals(' ', Cypher.getLetter(-1,4)),
-                ()-> assertEquals(' ', Cypher.getLetter(0,5)),
+                ()-> assertEquals('A', Cypher.getAlphabetLetter(0,0)),
+                ()-> assertEquals('E', Cypher.getAlphabetLetter(0,4)),
+                ()-> assertEquals('I', Cypher.getAlphabetLetter(1,3)),
+                ()-> assertEquals('K', Cypher.getAlphabetLetter(1,4)),
+                ()-> assertEquals('L', Cypher.getAlphabetLetter(2,0)),
+                ()-> assertEquals('P', Cypher.getAlphabetLetter(2,4)),
+                ()-> assertEquals('Z', Cypher.getAlphabetLetter(4,4)),
+                ()-> assertEquals(' ', Cypher.getAlphabetLetter(-1,4)),
+                ()-> assertEquals(' ', Cypher.getAlphabetLetter(0,5)),
 
                 ()-> {
                     int x = 2, y = 2; // 'H'
-                    assertFalse('Z' == Cypher.getLetter(x,y), "Z != "+ Cypher.getLetter(x,y));
+                    assertFalse('Z' == Cypher.getAlphabetLetter(x,y), "Z != "+ Cypher.getAlphabetLetter(x,y));
                 }
         );
 
