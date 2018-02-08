673fa11ce57aac18b6372f83cd0a6077a3790354
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-02-08 21:59:11 +0000

    ADD, IMPLEMENT, TEST, DOC: class Alphabet

--- src/main/java/ie/gmit/sw/Alphabet.java
+++ src/main/java/ie/gmit/sw/Alphabet.java
@@ -9,17 +9,39 @@ public class Alphabet {
     public static final int DEFAULT_LENGTH = 25;
     public static final String STANDARD_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    /**
+     * Flag to check if alphabet is standard;
+     */
     private boolean isStandard = false;
+
+    /**
+     * Square matrix (n*n) size (n).
+     */
     int matrixSize;
+
+    /**
+     * Alphabet of unique characters.
+     */
     char[] alphabet;
+
+    /**
+     * Alphabet data same as <i>char[] alphabet</i>. Letter is mapped to position of that letter.
+     */
     private Map<Character, Position> mapAlphabet = new HashMap<>();
 
+    /**
+     * Constructor for creating standard alphabet.
+     */
     private Alphabet(){
         this.alphabet = STANDARD_ALPHABET.replace("J", "").toCharArray();
         this.isStandard = true;
         this.matrixSize = (int)Math.sqrt(DEFAULT_LENGTH);
     }
 
+    /**
+     * Constructor for creating custom alphabet.
+     * @param alphabet custom alphabet data.
+     */
     private Alphabet(String alphabet){
         String str = MyUtils.createUniqueString(alphabet, ""); // remove duplicates
         int sqNum = (int)Math.sqrt(str.length()); // get square of that strings length
@@ -29,14 +51,36 @@ public class Alphabet {
         this.matrixSize = sqNum;
     }
 
+    /**
+     * <hr>
+     * <p>Factory method creates standard alphabet.</p>
+     * <hr>
+     * @return new instance of Alphabet class with standard alphabet.
+     */
     public static Alphabet of(){
         return new Alphabet();
     }
 
+    /**
+     * <hr>
+     * <p>Factory method creates custom alphabet.</p>
+     * <hr>
+     * @param alphabet should be used for creating object.
+     * @return new instance of Alphabet class with custom alphabet.
+     */
     public static Alphabet of(String alphabet){
         return new Alphabet(alphabet);
     }
 
+    /**
+     * <hr>
+     * <p>Method gets letter from standard alphabet.</p>
+     * <p>Alphabet letters are in square (n*n) matrix.</p>
+     * <hr>
+     * <i>Time complexity: <b>O(1)</b> - constant, <br> Space complexity: <b>O(0)</b> - none.</i>
+     * @param position position in matrix of the letter.
+     * @return letter from matrix at position (x,y) or "space" if not valid X or Y.
+     */
     public char get(Position position){
         if(isStandard){ return Alphabet.getFromStandard(position); }
 
@@ -46,7 +90,7 @@ public class Alphabet {
 
     /**
      * <hr>
-     * <p>Method gets letter from alphabet matrix.</p>
+     * <p>Method gets letter from standard alphabet.</p>
      * <ul style="list-style-type:none">
      *      <li>A B C D E</li>
      *      <li>F G H I K</li>
@@ -57,7 +101,6 @@ public class Alphabet {
      * <p><strong>NOTE: Here is no letter 'J' in Alphabet.</strong></p>
      * <hr>
      * <i>Time complexity: <b>O(1)</b> - constant, <br> Space complexity: <b>O(0)</b> - none.</i>
-     *
      * @param position position in matrix of the letter.
      * @return letter from matrix at position (x,y) or "space" if not valid X or Y.
      */
@@ -67,6 +110,15 @@ public class Alphabet {
         return (char)(5 * x + y + 'A' + (x > 1 || x == 1 && y == 4 ? 1 : 0));
     }
 
+    /**
+     * <hr>
+     * <p>Method gets position of the letter in alphabet.</p>
+     * <p><strong>NOTE: Here is no letter 'J' in standard alphabet. </strong></p>
+     * <hr>
+     * <i>Time complexity: <b>O(1)</b> - constant</i>
+     * @param letter letters position we look in matrix.
+     * @return Position of given letter or Optional.empty() if not found.
+     */
     Optional<Position> get(char letter){
         if( isStandard ){ return Alphabet.getFromStandard(letter); }
         return Optional.ofNullable(this.mapAlphabet.get(letter));
@@ -74,7 +126,8 @@ public class Alphabet {
 
     /**
      * <hr>
-     * <p>Method gets given letter Position (x,y) from alphabet matrix.</p>
+     * <p>Method gets position of the letter from standard alphabet.</p>
+     * <p><i>Example of standard alphabet:</i></p>
      * <ul style="list-style-type:none">
      *      <li>A B C D E</li>
      *      <li>F G H I K</li>
@@ -82,10 +135,9 @@ public class Alphabet {
      *      <li>Q R S T U</li>
      *      <li>V W X Y Z</li>
      * </ul>
-     * <p><strong>NOTE: Here is no letter 'J' in Alphabet.</strong></p>
+     * <p><strong>NOTE: Here is no letter 'J' in standard alphabet. </strong></p>
      * <hr>
      * <i>Time complexity: <b>O(1)</b> - constant, <br> Space complexity: <b>O(0)</b> - none.</i>
-     *
      * @param letter letters position we look in matrix.
      * @return Position of given letter or Optional.empty() if not found.
      */
---
+++ src/test/java/ie/gmit/sw/AlphabetTest.java
@@ -0,0 +1,57 @@
+package ie.gmit.sw;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlphabetTest {
+
+
+    @Test
+    @DisplayName("Get position")
+    void get() {
+        Alphabet abc = Alphabet.of();
+        Alphabet bca = Alphabet.of("QWERTYUIOPASDFGHJKLZXCVBNM");
+
+        assertAll(
+                ()-> assertEquals(Position.of(0,0), abc.get('A').get()),
+                ()-> assertEquals(Position.of(0,4), abc.get('E').get()),
+                ()-> assertEquals(Position.of(1,3), abc.get('I').get()),
+                ()-> assertEquals(Position.of(1,4), abc.get('K').get()),
+                ()-> assertEquals(Position.of(2,0), abc.get('L').get()),
+                ()-> assertEquals(Position.of(2,4), abc.get('P').get()),
+                ()-> assertEquals(Position.of(4,4), abc.get('Z').get()),
+
+                ()-> assertNotEquals(
+                        Position.of(2,2),
+                        abc.get('A').get(),
+                        "(2,2) != "+ abc.get('A').get()),
+                ()-> assertFalse( abc.get('J').isPresent() )
+        );
+    }
+
+    @Test
+    @DisplayName("Get letter")
+    void get1() {
+        Alphabet abc = Alphabet.of();
+
+        assertAll(
+                ()-> assertEquals('A', abc.get(Position.of(0,0))),
+                ()-> assertEquals('E', abc.get(Position.of(0,4))),
+                ()-> assertEquals('I', abc.get(Position.of(1,3))),
+                ()-> assertEquals('K', abc.get(Position.of(1,4))),
+                ()-> assertEquals('L', abc.get(Position.of(2,0))),
+                ()-> assertEquals('P', abc.get(Position.of(2,4))),
+                ()-> assertEquals('Z', abc.get(Position.of(4,4))),
+                ()-> assertEquals(' ', abc.get(Position.of(-1,4))),
+                ()-> assertEquals(' ', abc.get(Position.of(0,5))),
+
+                ()-> {
+                    int x = 2, y = 2; // 'H'
+                    assertFalse('Z' == abc.get(Position.of(x,y)), "Z != "+ abc.get(Position.of(x,y)));
+                }
+        );
+    }
+}
