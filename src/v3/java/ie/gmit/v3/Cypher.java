649d65a72f6bc70326a62b2079a1d87e63c78f09
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-02-27 13:29:07 +0000

    en-de lookup

--- src/v3/java/ie/gmit/v3/Cypher.java
+++ src/v3/java/ie/gmit/v3/Cypher.java
@@ -9,14 +9,29 @@ public class Cypher {
 
     static String alphabet = " ABCDEFGHIKLMNOPRSTUVWXYZ";
     char[] abc;
-    int[] enKey1, enKey2, deKey1, deKey2;
+    int[] enKey1, enKey2;
+    char[][][] deBigrams;
 
     private Cypher(String alphabet){
         this.abc = alphabet.toCharArray();
         this.enKey1 = generateRandomKey(this.abc.length);
         this.enKey2 = generateRandomKey(this.abc.length);
-        this.deKey1 = generateDecryptionKey(this.enKey1);
-        this.deKey2 = generateDecryptionKey(this.enKey2);
+        this.deBigrams = createDeBigrams(this.abc);
+    }
+
+    char[][][] createDeBigrams(char[] abc) {
+        char[][][] R = new char[abc.length][abc.length][];
+        final int limit = abc.length;
+
+        for(int x = 0; x < limit; x++){
+            for(int y = 0; y < limit; y++){
+                char[] encoded = encrypt(abc[x], abc[y]);
+                int enX = getIndexDefaultABC(encoded[0]);
+                int enY = getIndexDefaultABC(encoded[1]);
+                R[enX][enY] = new char[]{abc[x], abc[y]};
+            }
+        }
+        return R;
     }
 
     public static Cypher of(){
@@ -31,8 +46,9 @@ public class Cypher {
         Cypher cypher = new Cypher(alphabet);
         cypher.enKey1 = enKey1.codePoints().map(Cypher::getIndexDefaultABC).toArray();
         cypher.enKey2 = enKey2.codePoints().map(Cypher::getIndexDefaultABC).toArray();
-        cypher.deKey1 = deKey1.codePoints().map(Cypher::getIndexDefaultABC).toArray();
-        cypher.deKey2 = deKey2.codePoints().map(Cypher::getIndexDefaultABC).toArray();
+//        cypher.deKey1 = deKey1.codePoints().map(Cypher::getIndexDefaultABC).toArray();
+//        cypher.deKey2 = deKey2.codePoints().map(Cypher::getIndexDefaultABC).toArray();
+        cypher.deBigrams = cypher.createDeBigrams(cypher.abc);
 
         return cypher;
     }
@@ -78,8 +94,9 @@ public class Cypher {
 
     char[] decrypt(char ch1, char ch2) {
         int i1 = getIndexDefaultABC(ch1), i2 = getIndexDefaultABC(ch2);
-        int c1 = deKey1[(i1 / 5) * 5 + i2 % 5], c2 = deKey2[(i2 / 5) * 5 + i1 % 5];
-        return new char[]{abc[c1], abc[c2]};
+//        int c1 = deKey1[(i1 / 5) * 5 + i2 % 5], c2 = deKey2[(i2 / 5) * 5 + i1 % 5];
+//        return new char[]{abc[c1], abc[c2]};
+        return this.deBigrams[i1][i2];
     }
 
     static int getIndexDefaultABC(int i) {
@@ -96,8 +113,8 @@ public class Cypher {
                 "  \n   abc=" + Arrays.toString(abc) +
                 ", \nenKey1=[" + IntStream.of(enKey1).mapToObj(it-> abc[it] +"").collect(Collectors.joining(", ")) +
                 "], \nenKey2=[" + IntStream.of(enKey2).mapToObj(it-> abc[it] +"").collect(Collectors.joining(", ")) +
-                "], \ndeKey1=[" + IntStream.of(deKey1).mapToObj(it-> abc[it] +"").collect(Collectors.joining(", ")) +
-                "], \ndeKey2=[" + IntStream.of(deKey2).mapToObj(it-> abc[it] +"").collect(Collectors.joining(", ")) +
+//                "], \ndeKey1=[" + IntStream.of(deKey1).mapToObj(it-> abc[it] +"").collect(Collectors.joining(", ")) +
+//                "], \ndeKey2=[" + IntStream.of(deKey2).mapToObj(it-> abc[it] +"").collect(Collectors.joining(", ")) +
                 '}';
     }
 }
